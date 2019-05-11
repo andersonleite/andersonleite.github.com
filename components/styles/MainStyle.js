cfc283f49cfbcc3d7e9e1da7491971315f15dd06
Anderson Leite <anderson_leite@apple.com>
2019-05-11 14:53:49 -0700

    Sidebar layout with styled components.

---
+++ components/styles/MainStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MainStyle = styled.div`
+  margin-left: 25%; /* Same as the width of the sidenav */
+  font-size: 28px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+`
+
+export default MainStyle
---
+++ components/styles/SidenavStyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import theme from "../colors";
+
+const SidenavStyle = styled.div`
+  height: 100%;
+  width: 25%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: ${theme.grey_dark};
+  overflow-x: hidden;
+  padding-top: 20px;
+`
+
+export default SidenavStyle
