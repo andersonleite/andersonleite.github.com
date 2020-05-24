442d61bfebf2505f981bac8df0ecb1c6cc12e0e2
Anderson Leite <anderson_leite@apple.com>
2020-05-24 13:43:12 -0700

    photos

--- pages/photos.js
+++ pages/photos.js
@@ -8,40 +8,94 @@ export default function Photos() {
 
           <div className={`row`}>
             <div className="column">
+              <img src="/photography/blackbook/000.jpg" style={{width:"100%"}} />
               <img src="/photography/blackbook/001.jpg" style={{width:"100%"}} />
-                <img src="/photography/blackbook/002.jpg" style={{width:"100%"}} />
-                  <img src="/photography/blackbook/003.jpg" style={{width:"100%"}} />
-                        <img src="/photography/blackbook/023.jpg" style={{width:"100%"}} />
-                          <img src="/photography/blackbook/007.jpg" style={{width:"100%"}} />
-            </div>
-            <div className="column">
-              <img src="/photography/blackbook/008.jpg" style={{width:"100%"}} />
-                <img src="/photography/blackbook/009.jpg" style={{width:"100%"}} />
-                  <img src="/photography/blackbook/010.jpg" style={{width:"100%"}} />
-                    <img src="/photography/blackbook/011.jpg" style={{width:"100%"}} />
-                      <img src="/photography/blackbook/012.jpg" style={{width:"100%"}} />
-                        <img src="/photography/blackbook/013.jpg" style={{width:"100%"}} />
-            </div>
-            <div className="column">
-              <img src="/photography/blackbook/020.jpg" style={{width:"100%"}} />
-                <img src="/photography/blackbook/015.jpg" style={{width:"100%"}} />
-                  <img src="/photography/blackbook/016.jpg" style={{width:"100%"}} />
-                    <img src="/photography/blackbook/017.jpg" style={{width:"100%"}} />
-                      <img src="/photography/blackbook/018.jpg" style={{width:"100%"}} />
-                        <img src="/photography/blackbook/019.jpg" style={{width:"100%"}} />
-
-            </div>
-            <div className="column">
-              <img src="/photography/blackbook/023.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/022.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/021.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/014.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/003.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/004.jpg" style={{width:"100%"}} />
               <img src="/photography/blackbook/005.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/006.jpg" style={{width:"100%"}} />
-              <img src="/photography/blackbook/007.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/009.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/013.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/017.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/021.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/025.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/029.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/033.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/037.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/041.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/045.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/046.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/050.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/054.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/058.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/060.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/064.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/068.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/072.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/076.jpg" style={{width:"100%"}} />
 
+            </div>
+            <div className="column">
+              <img src="/photography/blackbook/002.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/006.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/010.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/014.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/018.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/022.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/026.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/030.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/034.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/038.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/042.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/047.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/051.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/055.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/059.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/061.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/065.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/069.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/073.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/077.jpg" style={{width:"100%"}} />
+            </div>
+            <div className="column">
+              <img src="/photography/blackbook/003.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/007.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/011.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/015.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/019.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/023.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/027.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/031.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/035.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/039.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/043.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/048.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/052.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/056.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/062.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/066.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/070.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/074.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/078.jpg" style={{width:"100%"}} />
+            </div>
+            <div className="column">
+              <img src="/photography/blackbook/004.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/008.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/012.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/016.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/020.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/024.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/028.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/032.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/036.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/040.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/044.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/049.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/053.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/057.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/063.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/067.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/071.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/075.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/079.jpg" style={{width:"100%"}} />
+              <img src="/photography/blackbook/080.jpg" style={{width:"100%"}} />
             </div>
           </div>
         </Layout>
